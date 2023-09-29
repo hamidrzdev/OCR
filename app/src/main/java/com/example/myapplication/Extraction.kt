b4d686f1586650ce75cca1b003d04b722742a18a
HamidReza <hrk1380@gmail.com>
2023-09-30 01:10:14 +0330

    some optimization

--- app/src/main/java/com/example/myapplication/Extraction.kt
+++ app/src/main/java/com/example/myapplication/Extraction.kt
@@ -6,7 +6,7 @@ import androidx.core.text.isDigitsOnly
 object Extraction {
     private const val TAG = "Extraction"
     var valueList = mutableListOf<String>()
-    operator fun invoke(value: String):CardDetail {
+    operator fun invoke(value: String): CardDetail {
         val list = value.split("\n").toMutableList()
         valueList = list
         Log.i(TAG, "invoke: list: $list")
@@ -23,7 +23,10 @@ object Extraction {
     private fun extractNumbers(): String {
         return try {
             Log.i(TAG, "extractNumbers: va $valueList")
-            val target = valueList.map { it.replace(" ", "") }.filter { it.isDigitsOnly() }
+            val target = valueList.map {
+                it.replace(" ", "")
+                    .replace(",", "")
+            }.filter { it.isDigitsOnly() }
             Log.i(TAG, "extractNumbers: target: $target")
 
             var cardNumber = target.find { it.length == 16 }
@@ -38,13 +41,18 @@ object Extraction {
     private fun extractCvv2(): String {
         return try {
             val target = valueList.find {
-                it.contains("c", true) or
-                        it.contains("v", true) or
-                        it.contains("w", true)
+                (it.contains("c", true) and it.hasDigit()) or
+                        (it.contains("v", true) and it.hasDigit()) or
+                        (it.contains("w", true) and it.hasDigit())
             } ?: ""
             Log.i(TAG, "extractCvv2: target: $target")
 
-            val cvv2List = target.split(" ")
+            var cvv2List = listOf<String>()
+            if (cvv2List.contains(" "))
+                cvv2List = target.split(" ") // cvv2: 422
+            else
+                cvv2List = target.split(":") // cvv2:422
+
             Log.i(TAG, "extractCvv2: cvv2List: $cvv2List")
             val digitTarget = cvv2List.filter { it.isDigitsOnly() }
 
@@ -92,4 +100,18 @@ object Extraction {
             ""
         }
     }
+}
+
+fun String.hasDigit(): Boolean {
+    Log.i("TAG", "hasDigit: source: $this")
+    val returnValue = false
+    val numbers = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map { it.toString() }
+    for (char: Char in this) {
+        numbers.forEach {
+            if (it == char.toString())
+                return true
+        }
+
+    }
+    return false
 }
